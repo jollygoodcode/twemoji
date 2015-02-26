04c178d1fc767fc766bf55eef818fe146ea2462c
Juanito Fatas <katehuang0320@gmail.com>
2015-02-27 00:40:12 +0800

    Reorganize test structure to match the order in code.

--- test/twemoji_test.rb
+++ test/twemoji_test.rb
@@ -5,20 +5,12 @@ class TwemojiTest < Minitest::Test
     @option = {}
   end
 
-  def test_find_by_text
-    assert_equal "1f60d", Twemoji.find_by_text(":heart_eyes:")
-  end
+  def test_finder_methods_cannot_find_by_more_than_one
+    exception = assert_raises ArgumentError do
+      Twemoji.find_by(text: ":heart_eyes:", code: "1f60d")
+    end
 
-  def test_find_by_code
-    assert_equal ":heart_eyes:", Twemoji.find_by_code("1f60d")
-  end
-
-  def test_find_by_unicode
-    assert_equal ":heart_eyes:", Twemoji.find_by_unicode("😍")
-  end
-
-  def test_find_by_escaped_unicode
-    assert_equal ":heart_eyes:", Twemoji.find_by_unicode("\u{1f60d}")
+    assert_equal "Can only specify text, code or unicode one at a time", exception.message
   end
 
   def test_finder_methods_find_by_text
@@ -33,12 +25,20 @@ class TwemojiTest < Minitest::Test
     assert_equal ":heart_eyes:", Twemoji.find_by(unicode: "😍")
   end
 
-  def test_finder_methods_cannot_find_by_more_than_one
-    exception = assert_raises ArgumentError do
-      Twemoji.find_by(text: ":heart_eyes:", code: "1f60d")
-    end
+  def test_find_by_text
+    assert_equal "1f60d", Twemoji.find_by_text(":heart_eyes:")
+  end
 
-    assert_equal "Can only specify text, code or unicode one at a time", exception.message
+  def test_find_by_code
+    assert_equal ":heart_eyes:", Twemoji.find_by_code("1f60d")
+  end
+
+  def test_find_by_unicode
+    assert_equal ":heart_eyes:", Twemoji.find_by_unicode("😍")
+  end
+
+  def test_find_by_escaped_unicode
+    assert_equal ":heart_eyes:", Twemoji.find_by_unicode("\u{1f60d}")
   end
 
   def test_parse_html_string
