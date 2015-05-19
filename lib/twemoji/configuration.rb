b823c6162b286f6a6d1b4a49bd3e3654268c81f3
Juanito Fatas <katehuang0320@gmail.com>
2015-05-19 17:43:28 +0900

    Implement Twemoji configuration

---
+++ lib/twemoji/configuration.rb
@@ -0,0 +1,25 @@
+module Twemoji
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configuration=(configuration)
+    @configuration = configuration
+  end
+
+  def self.configure
+    yield configuration
+  end
+
+  class Configuration
+    attr_accessor :asset_root, :file_ext, :image_size, :class_name, :img_attr
+
+    def initialize
+      @asset_root = "https://twemoji.maxcdn.com/".freeze
+      @file_ext   = ".png".freeze
+      @image_size = "16x16".freeze
+      @class_name = "emoji".freeze
+      @img_attr   = nil
+    end
+  end
+end
---
+++ test/configuration_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+class TwemojiConfigurationTest < Minitest::Test
+  def teardown
+    Twemoji.configuration = nil
+    Twemoji.configure {}
+  end
+
+  def test_configuration_defaults
+    assert_equal "https://twemoji.maxcdn.com/", Twemoji.configuration.asset_root
+    assert_equal ".png", Twemoji.configuration.file_ext
+    assert_equal "16x16", Twemoji.configuration.image_size
+    assert_equal "emoji", Twemoji.configuration.class_name
+    assert_equal nil, Twemoji.configuration.img_attr
+  end
+
+  def test_configuration_asset_root
+    Twemoji.configure do |config|
+      config.asset_root = "https://twemoji.awesomecdn.com/"
+    end
+
+    assert_equal "https://twemoji.awesomecdn.com/", Twemoji.configuration.asset_root
+  end
+
+  def test_configuration_file_ext
+    Twemoji.configure do |config|
+      config.file_ext = ".svg"
+    end
+
+    assert_equal ".svg", Twemoji.configuration.file_ext
+  end
+
+  def test_configuration_image_size
+    Twemoji.configure do |config|
+      config.file_ext = ".png"
+      config.image_size = "36x36"
+    end
+
+    assert_equal "36x36", Twemoji.configuration.image_size
+  end
+
+  def test_configuration_class_name
+    Twemoji.configure do |config|
+      config.class_name = "twemoji"
+    end
+
+    assert_equal "twemoji", Twemoji.configuration.class_name
+  end
+
+  def test_configuration_img_attr
+    Twemoji.configure do |config|
+      config.img_attr = "style='height: 1.3em;'"
+    end
+
+    assert_equal "style='height: 1.3em;'", Twemoji.configuration.img_attr
+  end
+end
